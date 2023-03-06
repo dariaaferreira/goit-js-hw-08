62e17cfeb7b7787f068b9e3910db4afdd3c45689
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-03-07 00:20:28 +0200

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const images = galleryItems.map(({ original, preview, description}) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+  </a>
+  </div>`
+}).join(" ");
+
+galleryContainer.innerHTML = images;
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+  /* options */ 
+  captions: true, 
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom'    
+});
