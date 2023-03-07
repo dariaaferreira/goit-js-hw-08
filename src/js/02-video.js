b13d2f785ab7931b397a4dbff13317b2d490c672
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-03-07 18:54:52 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo(iframe);
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data.seconds);
+};
+
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+
+iframePlayer
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    
+    })
+    .catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
